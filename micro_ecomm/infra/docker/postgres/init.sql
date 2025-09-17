2029bdf4da90c09b5d14b59ccb458acc24ddada6
quynh <quynh232000@gmail.com>
2025-09-17 23:01:23 +0700

    mount service into docker container

--- micro_ecomm/infra/docker/postgres/init.sql
+++ micro_ecomm/infra/docker/postgres/init.sql
@@ -1,4 +1,4 @@
--- Tạo DB chính cho microservice
+-- DB chính cho microservice
 CREATE DATABASE ecommerce;
 CREATE USER root WITH PASSWORD 'root';
 GRANT ALL PRIVILEGES ON DATABASE ecommerce TO root;
@@ -6,3 +6,39 @@ GRANT ALL PRIVILEGES ON DATABASE ecommerce TO root;
 CREATE DATABASE kong;
 CREATE USER kong WITH PASSWORD 'kong';
 GRANT ALL PRIVILEGES ON DATABASE kong TO kong;
+-- Auth Service
+CREATE DATABASE auth_service_db;
+CREATE USER auth_service_user WITH PASSWORD 'auth_pass';
+GRANT ALL PRIVILEGES ON DATABASE auth_service_db TO auth_service_user;
+-- Product Service
+CREATE DATABASE product_service_db;
+CREATE USER product_service_user WITH PASSWORD 'product_pass';
+GRANT ALL PRIVILEGES ON DATABASE product_service_db TO product_service_user;
+-- Cart Service
+CREATE DATABASE cart_service_db;
+CREATE USER cart_service_user WITH PASSWORD 'cart_pass';
+GRANT ALL PRIVILEGES ON DATABASE cart_service_db TO cart_service_user;
+-- Order Service
+CREATE DATABASE order_service_db;
+CREATE USER order_service_user WITH PASSWORD 'order_pass';
+GRANT ALL PRIVILEGES ON DATABASE order_service_db TO order_service_user;
+-- Payment Service
+CREATE DATABASE payment_service_db;
+CREATE USER payment_service_user WITH PASSWORD 'payment_pass';
+GRANT ALL PRIVILEGES ON DATABASE payment_service_db TO payment_service_user;
+-- Shipping Service
+CREATE DATABASE shipping_service_db;
+CREATE USER shipping_service_user WITH PASSWORD 'shipping_pass';
+GRANT ALL PRIVILEGES ON DATABASE shipping_service_db TO shipping_service_user;
+-- Notification Service
+CREATE DATABASE notification_service_db;
+CREATE USER notification_service_user WITH PASSWORD 'notification_pass';
+GRANT ALL PRIVILEGES ON DATABASE notification_service_db TO notification_service_user;
+-- Review Service
+CREATE DATABASE review_service_db;
+CREATE USER review_service_user WITH PASSWORD 'review_pass';
+GRANT ALL PRIVILEGES ON DATABASE review_service_db TO review_service_user;
+-- Search Service
+CREATE DATABASE search_service_db;
+CREATE USER search_service_user WITH PASSWORD 'search_pass';
+GRANT ALL PRIVILEGES ON DATABASE search_service_db TO search_service_user;
--- micro_ecomm/services/auth-service/src/main.ts
+++ micro_ecomm/services/auth-service/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port, '0.0.0.0');
+  console.log('App start s at: http://localhost:' + port);
 }
 bootstrap();
